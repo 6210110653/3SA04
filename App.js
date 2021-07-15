063b3a06efd74806710a7cebaac579fba69e6e8a
6210110653 <6210110653@psu.ac.th>
2021-07-15 10:24:21 +0700

    first commit

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const doIt = () => {
+  console.log("Hello Arm")
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <View style={styles.container}>
+  <Text onPress={doIt}>Hi Arm</Text>
+  <StatusBar style="auto" />
+  </View>
   );
-}
+ }
+ 
 
 const styles = StyleSheet.create({
   container: {
