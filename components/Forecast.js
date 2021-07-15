33ec4278983bb0e47c59865b946dc7bf22004766
6210110653 <6210110653@psu.ac.th>
2021-07-15 12:28:36 +0700

    section 4

--- components/Forecast.js
+++ components/Forecast.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function Forecast(props) {
     return (
         <View>
-            <Text>{props.mian}</Text>
+            <Text>{props.main}</Text>
             <Text>{props.description}</Text>
             <View>
                 <Text>{props.temp}</Text>
--- components/Weather.js
+++ components/Weather.js
@@ -1,18 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {ImageBackground, StyleSheet, Text} from 'react-native';
 import Forecast from './Forecast';
 
 export default function Weather(props) {
-    const [forecastInfo, setForecastInto] = useState({
+    const [ForecastInfo, setForecastInto] = useState({
         main: '-',
         description: '-',
         temp: 0
     }) 
+
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=6859d7f26aa4913deafa1c2e66f66a06`)
+            .then((response) => response.json())
+            .then((json) => {
+                setForecastInto({
+                    main: json.weather[0].main,
+                    description: json.weather[0].description,
+                    temp: json.main.temp
+                });
+            })
+            .catch((error) => {
+            console.warn(error);
+            });
+        }
+    }, [props.zipCode])
+       
+    
     return (
         <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
             <Text>Zip Code</Text>
             <Text>{props.zipCode}</Text>
-            <Forecast {...forecastInfo} />
+            <Forecast {...ForecastInfo} />
         </ImageBackground>
     )
 }
